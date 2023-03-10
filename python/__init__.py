6979d96f3a6de91ead6bf0cf8de050927551bf74
hero24 <hero24@interia.pl>
2023-03-10 12:54:30 +0000

    python: add setup file
    add setup file for python

---
+++ python/__init__.py
@@ -0,0 +1,25 @@
+"""
+    In the end, it's not the years in your life that count. It's the life in your years.
+    ~ Abraham Lincoln
+"""
+from CircularList import CircularList
+from queue import Queue
+
+__all__ = [
+    "Queue",
+    "CircularList"
+]
+
+if __name__ == "__main__":
+    from distutils.core import setup
+    from sys import argv
+    argv += ['install']
+
+    setup(name="data_structures",
+      version="0.1",
+      description="Python implementations of various data-sturctures",
+      author="hero24",
+      package_dir={'data_structures':'.'},
+      packages=["data_structures"]
+      )
+
